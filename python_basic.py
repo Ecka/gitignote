a7eecee1225e96951a117eadeaa9d2914bf8cc69
Edas Karpovicius <ecoedas@gmail.com>
2025-05-29 19:34:58 +0300

    1.0.2 version not workijg?

--- python_basic.py
+++ python_basic.py
@@ -1,4 +1,17 @@
+# Čia yra paprastas Komentaras, kuris bus išspausdintas
+
 print("hello world from python!")
-print("hello world from python!")
-print("hello world from python!")
-print("hello world from python!")
+# This is a string variable
+Message = "hello world from python!"
+# This is an integer variable
+Age = 18
+# This is a float variable
+Height = 19.99
+# This is a boolean variable
+is_married = False    
+# This is a list variable
+Name= "Edas"
+
+is_old_enough = Age > 18 # This is a boolean expression
+print(is_old_enough)
+# This is a print statement
